8ab00e2332f9095d695f0ed9fcbdbfb034d9e70f
Vansh Gandhi <vansh.gandhi@gmail.com>
2017-06-07 15:16:38 -0700

    Added more string extensions

--- src/main/kotlin/com/vanshgandhi/krazykotlin/StringExtensions.kt
+++ src/main/kotlin/com/vanshgandhi/krazykotlin/StringExtensions.kt
@@ -1,5 +1,8 @@
 package com.vanshgandhi.krazykotlin
 
+import java.net.MalformedURLException
+import java.net.URL
+import java.nio.charset.Charset
 import java.util.*
 
 /**
@@ -9,24 +12,24 @@ import java.util.*
 /**
  * Using UTF-8 encoding
  */
-fun String.base64Decoded() : String? {
-    return try {String(Base64.getDecoder().decode(toByteArray()))} catch (e: Exception) {null}
+fun String.base64Decoded() = try {
+    String(Base64.getDecoder().decode(toByteArray()))
+} catch (e: IllegalArgumentException) {
+    null
 }
 
 /**
  * Using UTF-8 encoding
  */
-fun String.base64Encoded() : String {
-    return Base64.getEncoder().encodeToString(toByteArray())
-}
+fun String.base64Encoded() = Base64.getEncoder().encodeToString(toByteArray())
 
 /**
  * Splits by spaces, newlines, and tabs only
  */
-fun String.camelCased() : String {
+fun String.camelCased(): String {
     val split = toLowerCase().split(' ', '\n', '\t').toMutableList()
     if (split.size > 1) {
-        for (i in 1 .. split.size-1) {
+        for (i in 1..split.size - 1) {
             if (split[i].length > 1) {
                 val charArray = split[i].toCharArray()
                 charArray[0] = charArray[0].toUpperCase()
@@ -37,3 +40,35 @@ fun String.camelCased() : String {
     return split.joinToString("")
 }
 
+fun String.containsLetters() = matches(".*[a-zA-Z].*".toRegex())
+
+fun String.containsNumbers() = matches(".*[0-9].*".toRegex())
+
+/**
+ * Does not allow whitespace or symbols
+ * Allows empty string
+ */
+fun String.isAlphanumeric() = matches("^[a-zA-Z0-9]*$".toRegex())
+
+/**
+ * Does not allow whitespace or symbols
+ * Allows empty string
+ */
+fun String.isAlphabetic() = matches("^[a-zA-Z]*$".toRegex())
+
+/**
+ * Does not allow whitespace or symbols
+ * Allows empty string
+ */
+fun String.isNumeric() = matches("^[0-9]*$".toRegex())
+
+fun String.isEmail() = matches("[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}".toRegex())
+
+/**
+ * Only matches URLs starting with a scheme (e.g. http://, file://, ftp://, etc)
+ */
+fun String.isUrl() = try {
+    URL(this) != null
+} catch (e: MalformedURLException) {
+    false
+}
--- src/test/kotlin/com/vanshgandhi/krazykotlin/StringTest.kt
+++ src/test/kotlin/com/vanshgandhi/krazykotlin/StringTest.kt
@@ -2,7 +2,9 @@ package com.vanshgandhi.krazykotlin
 
 import org.junit.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 /**
  * Created by Vansh Gandhi on 6/7/17.
@@ -24,4 +26,57 @@ class StringTest {
         assertEquals("Helloworld\t".camelCased(), "helloworld")
     }
 
+    @Test fun testContainsLetters() {
+        assertTrue("1 Hello, World!".containsLetters())
+        assertFalse("123".containsLetters())
+    }
+
+    @Test fun testContainsNumbers() {
+        assertTrue("A 123".containsNumbers())
+        assertFalse("Hello, World!".containsNumbers())
+    }
+
+    @Test fun testIsAlphanumeric() {
+        assertTrue("ABC123".isAlphanumeric())
+        assertTrue("".isAlphanumeric())
+        assertTrue("abc".isAlphanumeric())
+        assertTrue("123".isAlphanumeric())
+        assertFalse("abc 123".isAlphanumeric())
+        assertFalse("123!".isAlphanumeric())
+    }
+
+    @Test fun testIsAlphabetic() {
+        assertTrue("abcXYZ".isAlphabetic())
+        assertTrue("".isAlphabetic())
+        assertFalse("ABC123".isAlphabetic())
+        assertFalse("abc ".isAlphabetic())
+        assertFalse("123".isAlphabetic())
+        assertFalse("abc!".isAlphabetic())
+    }
+
+    @Test fun testIsNumeric() {
+        assertTrue("123".isNumeric())
+        assertTrue("".isNumeric())
+        assertFalse("abcXYZ".isNumeric())
+        assertFalse("ABC123".isNumeric())
+        assertFalse("123 ".isNumeric())
+        assertFalse("123!".isNumeric())
+    }
+
+    @Test fun testIsEmail() {
+        assertTrue("vansh.gandhi@gmail.com".isEmail())
+        assertFalse("vansh.gandhigmail.com".isEmail())
+        assertFalse("vansh.gandhi@gmailcom".isEmail())
+        assertFalse("".isEmail())
+    }
+
+    @Test fun testIsUrl() {
+        assertTrue("http://van.sh".isUrl())
+        assertTrue("https://van.sh".isUrl())
+        assertTrue("https://www.van.sh".isUrl())
+        assertTrue("http://van.sh/resume.pdf".isUrl())
+        assertTrue("file://test1/test2/file.txt".isUrl())
+        assertFalse("Hello, world".isUrl())
+        assertFalse("".isUrl())
+    }
 }
