3eaa4642e8f837420bd01b6f9d99fd7ffb0e9587
Vansh Gandhi <vansh.gandhi@gmail.com>
2017-08-02 20:13:33 -0700

    Update kotlin version

--- src/test/kotlin/com/vanshgandhi/krazykotlin/BooleanExtensionsTest.kt
+++ src/test/kotlin/com/vanshgandhi/krazykotlin/BooleanExtensionsTest.kt
@@ -1,7 +1,9 @@
 package com.vanshgandhi.krazykotlin
 
-import org.junit.Assert.assertEquals
 import org.junit.Test
+import org.junit.Assert.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 /**
  * Created by Vansh Gandhi on 6/8/17.
@@ -11,5 +13,10 @@ class BooleanTest {
     @Test fun testIntValue() {
         assertEquals(true.intValue, 1)
         assertEquals(false.intValue, 0)
+    }
+
+    @Test fun testToggled() {
+        assertFalse(true.toggled)
+        assertTrue(false.toggled)
     }
 }
---
+++ src/test/kotlin/com/vanshgandhi/krazykotlin/IntExtensionsTest.kt
@@ -0,0 +1,5 @@
+package com.vanshgandhi.krazykotlin
+
+/**
+ * Created by Vansh Gandhi on 8/2/17.
+ */
